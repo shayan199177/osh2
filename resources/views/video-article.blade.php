a6e438180030dba695ec76b5925c5b491343dd1d
Shayan199177 <sh.salimzadeh@gmail.com>
2021-03-10 20:50:37 +0330

    videos.blade.php

--- resources/views/video-article.blade.php
+++ resources/views/video-article.blade.php
@@ -202,13 +202,13 @@
                 <nav aria-label="Page navigation example">
                     <ul class="pagination justify-content-center">
                         <li class="page-item">
-                            <a class="page-link" href="#" tabindex="-1">بعدی</a>
+                            <a class="page-link gray-color" href="#" tabindex="-1">بعدی</a>
                         </li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link gray-color" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link gray-color" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link gray-color" href="#">1</a></li>
                         <li class="page-item">
-                            <a class="page-link disabled" href="#">قبلی</a>
+                            <a class="page-link gray-color disabled" href="#">قبلی</a>
                         </li>
                     </ul>
                 </nav>
